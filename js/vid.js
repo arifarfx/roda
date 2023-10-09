4c0f16365560d08f9e59e6497faddba9d5a15a47
arifarfx <arifarforex@gmail.com>
2023-10-10 02:50:12 +0700

    Update vid.js

--- js/vid.js
+++ js/vid.js
@@ -14,26 +14,32 @@ function getJsonData(url) {
 // Dapatkan data JSON
 var jsonData = getJsonData("https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/applixir.json");
 
-// Ganti nilai nnnn
-var options = {
-    zoneId: jsonData.zoneid,
-    devId: jsonData.accid,
-    gameId: jsonData.gameid,
-    userId: wp.currentUser.id,
-    custom: "Video Rewards",
-    adStatusCb: adStatusCallback,
-};
+// Periksa apakah variabel jsonData terdefinisi dan memiliki properti zoneid
+if (jsonData && jsonData.hasOwnProperty("zoneid")) {
+    // Ganti nilai nnnn
+    var options = {
+        zoneId: jsonData.zoneid,
+        devId: jsonData.accid,
+        gameId: jsonData.gameid,
+        userId: wp.currentUser.id,
+        custom: "Video Rewards",
+        adStatusCb: adStatusCallback,
+    };
 
-// Invoke Applixir Video Unit
-invokeApplixirVideoUnit(options);
+    // Invoke Applixir Video Unit
+    invokeApplixirVideoUnit(options);
 
-// Dapatkan status Applixir
-var status = adStatusCallback();
+    // Dapatkan status Applixir
+    var status = adStatusCallback();
 
-// Kirim data log ke file vidrewards.php
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/vidrewards.php", true);
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(JSON.stringify({
-    status: status,
-}));
+    // Kirim data log ke file vidrewards.php
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/vidrewards.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        status: status,
+    }));
+} else {
+    // Tampilkan pesan kesalahan
+    alert("Kesalahan: Data JSON tidak valid");
+}
