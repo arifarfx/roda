f03eba107c56bfdcd7e96fa1ab9e451edeea993d
arifarfx <arifarforex@gmail.com>
2023-10-10 03:11:38 +0700

    Update vid.js
    ubah1

--- js/vid.js
+++ js/vid.js
@@ -1,45 +1,50 @@
 function getJsonData(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            return JSON.parse(xhr.responseText);
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+getJsonData("https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/jsongen.php")
+    .then(jsonData => {
+        if (jsonData && jsonData.hasOwnProperty("zoneid")) {
+            var options = {
+                zoneId: jsonData.zoneid,
+                devId: jsonData.accid,
+                gameId: jsonData.gameid,
+                userId: wp.currentUser.id,
+                custom: "Video Rewards",
+                adStatusCb: adStatusCallback,
+            };
+            invokeApplixirVideoUnit(options);
+            var status = adStatusCallback();
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/vidrewards.php", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify({
+                status: status,
+            }));
         } else {
-            return null;
+            alert("Kesalahan: Data JSON tidak valid");
         }
-    };
-    xhr.send();
-}
-
-// Dapatkan data JSON
-var jsonData = getJsonData("https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/jsongen.php");
-
-// Periksa apakah variabel jsonData terdefinisi dan memiliki properti zoneid
-if (jsonData && jsonData.hasOwnProperty("zoneid")) {
-    // Ganti nilai nnnn
-    var options = {
-        zoneId: jsonData.zoneid,
-        devId: jsonData.accid,
-        gameId: jsonData.gameid,
-        userId: wp.currentUser.id,
-        custom: "Video Rewards",
-        adStatusCb: adStatusCallback,
-    };
-
-    // Invoke Applixir Video Unit
-    invokeApplixirVideoUnit(options);
-
-    // Dapatkan status Applixir
-    var status = adStatusCallback();
-
-    // Kirim data log ke file vidrewards.php
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://clubmagneto.com/wp-content/plugins/gamiwheel/inc/vidrewards.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({
-        status: status,
-    }));
-} else {
-    // Tampilkan pesan kesalahan
-    alert("Kesalahan: Data JSON tidak valid");
-}
+    })
+    .catch(error => {
+        alert("Kesalahan: " + error.statusText);
+    });
